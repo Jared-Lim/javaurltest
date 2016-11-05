cd3b546a7448ca891fa5e708cad330388143005f
Jared <jaredlgw@outlook.com>
2016-11-05 18:40:57 +1100

    cleanup pt. 3

--- src/urltest3.java
+++ src/urltest3.java
@@ -8,9 +8,6 @@ public class urltest3 {
 		URL url;
 		URLConnection urlc;
 		StringBuilder html = new StringBuilder();
-		//String urlstr = "http://freemusicarchive.org/api/get/albums.json?api_key=60BLHNQCAOUFPIBZ&limit=5";
-		
-		System.out.println("Analyzing the following url:\n" + urlstr);
 		
 		url = new URL(urlstr);
 		urlc = url.openConnection();
@@ -23,7 +20,7 @@ public class urltest3 {
 		while ((ch = in.read()) != -1) {
 		    html.append((char) ch);
 		}
-		//System.out.println(html);
-		return html;
+		
+		return html.toString();
 	}
 }
