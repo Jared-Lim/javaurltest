2e833436fae61820fb23598b0d88481155adb048
Jared <jaredlgw@outlook.com>
2016-11-05 18:29:29 +1100

    cleanup pt2

--- src/urltest3.java
+++ src/urltest3.java
@@ -7,17 +7,17 @@ public class urltest3 {
 	public static void main(String[] args) throws IOException {
 		URL url;
 		URLConnection urlc;
+		StringBuilder html = new StringBuilder();
 		String urlstr = "http://freemusicarchive.org/api/get/albums.json?api_key=60BLHNQCAOUFPIBZ&limit=5";
 		
-		System.out.println("Analyzing the following string:\n" + urlstr);
+		System.out.println("Analyzing the following url:\n" + urlstr);
 		
 		url = new URL(urlstr);
 		urlc = url.openConnection();
 		urlc.connect();
 		urlc = url.openConnection();
 		urlc.setRequestProperty("User-Agent", "Mozilla/5.0");
-		StringBuilder html = new StringBuilder();
-
+		
 		BufferedInputStream in = new BufferedInputStream(urlc.getInputStream());
 		int ch;
 		while ((ch = in.read()) != -1) {
